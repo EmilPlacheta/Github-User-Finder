e9546fbb084e25682df897e4793590ee8013aa82
Emil Placheta <emil.minoru@gmail.com>
2019-08-19 16:05:50 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment, Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Navbar />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
